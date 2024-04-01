0a12fa9c329ee90228d5766119eeec8e39d71d81
Alex49er <151443937+Alex49er@users.noreply.github.com>
2024-04-01 10:55:39 +0100

    installed react router 5 not the latest version for compatitbility

--- movie/src/App.js
+++ movie/src/App.js
@@ -2,16 +2,21 @@ import './App.css';
 import Navbar from './components/Navbar';
 import Home from './components/Home'
 import AboutView from './components/AboutView'
-
+import {Switch, Route} from 'react-router-dom';
 
 
 function App() {
   return (
     <div>
       <Navbar/>
-      <Home/>
+      <Switch>
+      <Route path ="/" exact>
+        <Home/>
+      </Route>
+      <Route path="/about" component={AboutView}/>
+         
       <AboutView/>
-      
+      </Switch>
     </div>
   );
 }
