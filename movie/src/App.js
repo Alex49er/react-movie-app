7f214b56e99d0f53753482414f434910a76b4e57
Alex49er <151443937+Alex49er@users.noreply.github.com>
2024-04-01 15:17:16 +0100

    included movie view component, styles and dynamic responsivness, fetched using api key

--- movie/src/App.js
+++ movie/src/App.js
@@ -4,6 +4,7 @@ import Navbar from './components/Navbar';
 import Home from './components/Home'
 import AboutView from './components/AboutView'
 import SearchView from './components/SearchView';
+import MovieView from './components/MovieView';
 import {Switch, Route} from 'react-router-dom';
 
 
@@ -11,6 +12,15 @@ function App() {
 
   const [searchResults, setSearchResults] = useState([])
   const [searchText, setSearchText] = useState('')
+
+  useEffect(() => {
+    console.log(searchText, "is the search text")
+    fetch(`http://api.themoviedb.org/3/search/movie?api_key=107708d07fb59d7d5b7586af1aad916d&language=en-US&query=${searchText}&page=1&include_adult=false`)
+    .then(resonse => resonse.json())
+    .then(data => {
+      setSearchResults(data.results)
+    })
+  }, [searchText])
 
 
   return (
@@ -24,7 +34,7 @@ function App() {
          <Route path="/search">
           <SearchView keyword={searchText} searchResults={searchResults}/>
          </Route>
-      <AboutView/>
+      <Route path="/movies/:id" component={MovieView}/>
       </Switch>
     </div>
   );
